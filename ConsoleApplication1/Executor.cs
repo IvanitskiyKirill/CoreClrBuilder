4f6097f2381e8d355e7afe61ce611fdcfe29fb8b
unknown <ivanitskiy.kirill@IVANITSKIY-W8.corp.devexpress.com>
2015-08-07 15:09:01 +0300

    merge nunit results in one file

--- ConsoleApplication1/Executor.cs
+++ ConsoleApplication1/Executor.cs
@@ -8,6 +8,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Linq;
 using System.Xml.Xsl;
 
 namespace CoreClrBuilder
@@ -130,6 +131,7 @@ namespace CoreClrBuilder
             XslCompiledTransform xslt = new XslCompiledTransform();
             xslt.Load("NUnitXml.xslt");
 
+            List<string> nUnitTestFiles = new List<string>();
             foreach (var project in productInfo.Projects)
             {
                 string xUnitResults = Path.Combine(WorkingDir, project.TestResultFileName);
@@ -139,12 +141,17 @@ namespace CoreClrBuilder
                     File.Delete(xUnitResults);
 
                 result += DoWork(dnxPath, string.Format(@"{0} test -xml {1}", project.LocalPath, project.TestResultFileName));
-                
+
                 if (File.Exists(nUnitResults))
                     File.Delete(nUnitResults);
                 if (File.Exists(xUnitResults))
+                {
                     xslt.Transform(xUnitResults, nUnitResults);
+                    nUnitTestFiles.Add(nUnitResults);
+                }
             }
+
+            NUnitMerger.MergeFiles(nUnitTestFiles, "nunit-result.xml");
             return result;
         }
 
@@ -271,17 +278,81 @@ namespace CoreClrBuilder
         }
     }
 
-    //class ProjectBuilder {
-    //    CoreClrProject project;
-    //    public ProjectBuilder(CoreClrProject project)
-    //    {
+    public static class NUnitMerger
+    {
+        public static bool MergeFiles(IEnumerable<string> files, string output)
+        {
+            XElement environment = null;
+            XElement culture = null;
+            var suites = new List<XElement>();
 
-    //    }
+            bool finalSuccess = true;
+            string finalResult = "";
+            double totalTime = 0;
+            int total = 0, errors = 0, failures = 0, notrun = 0, inconclusive = 0, ignored = 0, skipped = 0, invalid = 0;
+            foreach (var file in files)
+            {
+                if (!File.Exists(file))
+                    continue;
+                var doc = XDocument.Load(file);
+                var tr = doc.Element("test-results");
 
-    //    public void Build() {
+                if (environment == null)
+                    environment = tr.Element("environment");
+                if (culture == null)
+                    culture = tr.Element("culture-info");
 
-    //    }
-    //}
+                if (tr.Attribute("total") != null)
+                    total += Convert.ToInt32(tr.Attribute("total").Value);
+                if (tr.Attribute("errors") != null)
+                    errors += Convert.ToInt32(tr.Attribute("errors").Value);
+                if (tr.Attribute("failures") != null)
+                    failures += Convert.ToInt32(tr.Attribute("failures").Value);
+                if (tr.Attribute("not-run") != null)
+                    notrun += Convert.ToInt32(tr.Attribute("not-run").Value);
+                if (tr.Attribute("inconclusive") != null)
+                    inconclusive += Convert.ToInt32(tr.Attribute("inconclusive").Value);
+                if (tr.Attribute("ignored") != null)
+                    ignored += Convert.ToInt32(tr.Attribute("ignored").Value);
+                if (tr.Attribute("skipped") != null)
+                    skipped += Convert.ToInt32(tr.Attribute("skipped").Value);
+                if (tr.Attribute("invalid") != null)
+                    invalid += Convert.ToInt32(tr.Attribute("invalid").Value);
+
+                var ts = tr.Element("test-suite");
+                //string result = ts.Attribute("result").Value;
+
+                if (!Convert.ToBoolean(ts.Attribute("success").Value))
+                    finalSuccess = false;
+
+                totalTime += Convert.ToDouble(ts.Attribute("time").Value);
+
+                //if (finalResult != "Failure" && (String.IsNullOrEmpty(finalResult) || result == "Failure" || finalResult == "Success"))
+                //    finalResult = result;
+
+                suites.Add(ts);
+            }
+
+            if (String.IsNullOrEmpty(finalResult))
+            {
+                finalSuccess = false;
+                finalResult = "Inconclusive";
+            }
+
+            var project = XElement.Parse(String.Format("<test-suite type=\"Test Project\" name=\"\" executed=\"True\" result=\"{0}\" success=\"{1}\" time=\"{2}\" asserts=\"0\" />", finalResult, finalSuccess ? "True" : "False", totalTime));
+            var results = XElement.Parse("<results/>");
+            results.Add(suites.ToArray());
+            project.Add(results);
+
+            var now = DateTime.Now;
+            var trfinal = XElement.Parse(String.Format("<test-results name=\"Merged results\" total=\"{0}\" errors=\"{1}\" failures=\"{2}\" not-run=\"{3}\" inconclusive=\"{4}\" ignored=\"{5}\" skipped=\"{6}\" invalid=\"{7}\" date=\"{8}\" time=\"{9}\" />", total, errors, failures, notrun, inconclusive, ignored, skipped, invalid, now.ToString("yyyy-MM-dd"), now.ToString("HH:mm:ss")));
+            trfinal.Add(new[] { environment, culture, project });
+            trfinal.Save(output);
+
+            return finalSuccess;
+        }
+
+    }
 
     class CoreClrCommand {
         public string FileName { get; private set; }
