9706d626736cbc7f1911b0c5c470a857fda79d83
Kirillka <kivanitskiy88@gmail.com>
2015-09-21 12:05:41 +0300

    orginize using

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,16 +1,5 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Xml;
-using System.Xml.Xsl;
 
 namespace CoreClrBuilder
 {
@@ -53,4 +42,5 @@ namespace CoreClrBuilder
             return executor.ExecuteTasks(dnxSettings, stepSettings, envSettings);
         }
     }
+
 }
