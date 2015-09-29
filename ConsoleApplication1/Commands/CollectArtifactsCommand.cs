bf36609eaf1e745c568ad9887c473d90d50db777
Kirillka <kivanitskiy88@gmail.com>
2015-09-29 19:27:44 +0300

    collect arrifacts

--- ConsoleApplication1/Commands/CollectArtifactsCommand.cs
+++ ConsoleApplication1/Commands/CollectArtifactsCommand.cs
@@ -8,9 +8,10 @@ namespace CoreClrBuilder.Commands
         ProjectsInfo info;
         string destFolder;
         EnvironmentSettings settings;
-
+        
         public CollectArtifactsCommand(EnvironmentSettings settings, ProjectsInfo info, string destFolder, string buildFramework)
         {
+            
             this.settings = settings;
             this.info = info;
             if (!string.IsNullOrEmpty(buildFramework))
@@ -20,21 +21,32 @@ namespace CoreClrBuilder.Commands
         }
         public void Execute()
         {
-            if (Directory.Exists(destFolder))
-                Directory.Delete(destFolder, true);
-            Directory.CreateDirectory(destFolder);
-
-            foreach (var project in info.Projects)
+            try
             {
-                //string localPackagePath = PlatformPathsCorrector.Inst.Correct(string.Format(@"{0}\bin\{1}\{2}", project.LocalPath, project.BuildConfiguration, project.NugetPackageName), Platform.Windows);
-                string localPackagePath = Path.Combine(settings.PackagesPath, project.ProjectName);
-                if (Directory.Exists(localPackagePath))
+                if (Directory.Exists(destFolder))
                 {
-                    Console.WriteLine("Start copy package {0}", project.NugetPackageName);
-                    CopyProjectsCommand.DirectoryCopy(localPackagePath, Path.Combine(destFolder, project.ProjectName), true);
+                    foreach (var folder in Directory.GetDirectories(destFolder))
+                        Directory.Delete(folder, true);
                 }
-                else
-                    Console.WriteLine("Package {0} doesn't exist", project.NugetPackageName);
+                Directory.CreateDirectory(destFolder);
+
+                Console.WriteLine("Copy packages to {0}", destFolder);
+                foreach (var project in info.Projects)
+                {
+                    //string localPackagePath = PlatformPathsCorrector.Inst.Correct(string.Format(@"{0}\bin\{1}\{2}", project.LocalPath, project.BuildConfiguration, project.NugetPackageName), Platform.Windows);
+                    string localPackagePath = Path.Combine(settings.PackagesPath, project.ProjectName);
+                    if (Directory.Exists(localPackagePath))
+                    {
+                        Console.WriteLine("Start copy package {0}", project.NugetPackageName);
+                        CopyProjectsCommand.DirectoryCopy(localPackagePath, Path.Combine(destFolder, project.ProjectName), true);
+                    }
+                    else
+                        Console.WriteLine("Package {0} doesn't exist", project.NugetPackageName);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
             }
         }
     }
