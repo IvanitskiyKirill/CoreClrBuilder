22f512abba1e344e74ef6ae9c4b0dcb90bfc8c8d
Vyacheslav Zaikin (DevExpress) <vyacheslav.zaikin@devexpress.com>
2016-01-22 16:36:16 +0300

    CollectArtifacts path now includes runtime id

--- ConsoleApplication1/Commands/CollectArtifactsCommand.cs
+++ ConsoleApplication1/Commands/CollectArtifactsCommand.cs
@@ -15,7 +15,7 @@ namespace CoreClrBuilder.Commands
             this.settings = settings;
             this.info = info;
             if (!string.IsNullOrEmpty(buildFramework) && !string.IsNullOrEmpty(runtime))
-                this.destFolder = string.Format(@"{0}\{1}\{2}", destFolder, runtime, buildFramework);
+                this.destFolder = PlatformPathsCorrector.Inst.Correct(string.Format(@"{0}\{1}\{2}", destFolder, runtime, buildFramework), Platform.Windows);
             else
                 this.destFolder = destFolder;
         }
